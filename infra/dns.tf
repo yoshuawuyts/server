ae57b9ea1fe50322afd1ab09075785f6f549a2d0
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-10-22 09:50:00 +0200

    add datrs.yoshuawuyts.com
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- infra/dns.tf
+++ infra/dns.tf
@@ -3,6 +3,14 @@ resource "digitalocean_domain" "default" {
   ip_address = "${digitalocean_droplet.web.ipv4_address}"
 }
 
+resource "digitalocean_record" "A-stats" {
+  domain = "${digitalocean_domain.default.name}"
+  type = "A"
+  name = "stats"
+  value = "${digitalocean_droplet.web.ipv4_address}"
+  ttl = 3600
+}
+
 resource "digitalocean_record" "CNAME-www" {
   domain = "${digitalocean_domain.default.name}"
   type = "CNAME"
@@ -11,10 +19,10 @@ resource "digitalocean_record" "CNAME-www" {
   ttl = 3600
 }
 
-resource "digitalocean_record" "A-stats" {
+resource "digitalocean_record" "CNAME-datrs" {
   domain = "${digitalocean_domain.default.name}"
-  type = "A"
-  name = "stats"
-  value = "${digitalocean_droplet.web.ipv4_address}"
+  type = "CNAME"
+  name = "datrs"
+  value = "datrs.netlify.com."
   ttl = 3600
 }
